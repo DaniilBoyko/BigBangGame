e67027705eb54c52299b5132eb548ff54d3602a0
Daniil <daniil.boyko.2017@gmail.com>
2023-02-04 23:19:24 +0300

    Add BigBangGame.Server.Tests project

---
+++ BigBangGame.Server.Tests/Services/ChoiceServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using BigBangGame.Server.Mapping;
+using BigBangGame.Server.Services;
+using BigBangGame.Server.Services.Interfaces;
+using FluentAssertions;
+
+namespace BigBangGame.Server.Tests.Services;
+
+internal class ChoiceServiceTests
+{
+    private readonly IChoiceService _sut;
+
+    public ChoiceServiceTests()
+    {
+        var mapper = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new MappingProfile());
+        }).CreateMapper();
+
+        _sut = new ChoiceService(mapper);
+    }
+
+    [Test]
+    public void GetAvailableChoices_Rerun5Choices()
+    {
+        var choices = _sut.GetAvailableChoices().ToList();
+
+        choices.Should().HaveCount(5);
+    }
+
+    [Test]
+    [TestCase(1, "rock")]
+    [TestCase(2, "paper")]
+    [TestCase(3, "scissors")]
+    [TestCase(4, "spock")]
+    [TestCase(5, "lizard")]
+    public void GetAvailableChoices_ShouldHaveCorrectNames(int choiceId, string name)
+    {
+        var choices = _sut.GetAvailableChoices().ToList();
+        var choice = choices.FirstOrDefault(x => x.Id == choiceId);
+
+        choice.Should().NotBeNull();
+        choice!.Name.Should().Be(name);
+    }
+}
